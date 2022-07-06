a3c0353fdcac8275afe66f7134b5d3a903c7222a
jaykhans <jahongirnagmatov@gmail.com>
2022-07-06 12:39:59 +0500

    create simple counter with redux

--- src/app/store.js
+++ src/app/store.js
@@ -1,4 +1,4 @@
-import {configureStore} from '@rduxjs/toolkit';
+import {configureStore} from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 
 export default configureStore({
